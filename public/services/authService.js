60eab3dfb5a98b46478fe0fd9c7f6da680213fcd
amirg0201 <amir.garcia@udla.edu.ec>
2025-12-20 23:06:05 -0500

    chore: fix headers

--- public/services/authService.js
+++ public/services/authService.js
@@ -4,13 +4,14 @@ export const loginUser = async (email, password) => {
     const response = await fetch(`${API_BASE_URL}/usuarios/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })     
     });
     return response;
 };
 
 export const registerUser = async (userData) => {
 
-    const response = await fetch(`${API_BASE_URL}/usuarios`, {
+    const response = await fetch(`${API_BASE_URL}/usuarios/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userData)
